97350be5d618a4f6709c7284d3e3d780c802dfdb
github username <sharnellgrant@gmail.com>
2021-02-02 12:38:31 -0500

    Trying to get my scope method to work

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -2,5 +2,5 @@ class Raffle < ApplicationRecord
     belongs_to :shoe, optional: true  #raffles.shoe_id
     belongs_to :user
     accepts_nested_attributes_for :user
-
+  
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,7 @@
 class Shoe < ApplicationRecord
     has_many :raffles
     has_many :users, through: :raffles
+    scope :high_priced_item, -> (amount = $150) { where('price > ?', amount) }
 end
+
+#scope :future_appointments, -> {where("start_time > ?", Time.now ).order('start_time asc')}
